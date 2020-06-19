9cb0fd320acf36800a31725cfa3d15414d5ee6d2
Mykyta Kozhemiakin <knyazvladimir26@gmail.com>
2020-06-19 16:56:12 +0200

    added removeItem from checkout by click on cross symbol feature

--- src/redux/cart/cart-action.js
+++ src/redux/cart/cart-action.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: cartActions.ADD_ITEM_TO_CART,
   payload: item,
 });
+
+export const removeItemsFromCart = (item) => ({
+  type: cartActions.REMOVE_ITEM_FROM_CART,
+  payload: item,
+});
--- src/redux/cart/cart-constants.js
+++ src/redux/cart/cart-constants.js
@@ -1,4 +1,5 @@
 export const cartActions = {
     TOGGLE_CART: "TOGGLE_CART",
     ADD_ITEM_TO_CART: "ADD_ITEM_TO_CART",
+    REMOVE_ITEM_FROM_CART:' REMOVE_ITEM_FROM_CART'
   };
