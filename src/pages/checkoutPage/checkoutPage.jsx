3bd4a5867921ec2a53575c3587482482092f1b91
Mykyta Kozhemiakin <knayzvladimir26@gmail.com>
2020-10-23 19:34:12 +0200

    added payment with stripes API

--- src/pages/checkoutPage/checkoutPage.jsx
+++ src/pages/checkoutPage/checkoutPage.jsx
@@ -6,6 +6,7 @@ import {
   selectCartTotalSum,
 } from "../../redux/cart/cart-selector";
 
+import TakeMoney from "../../components/stripe-button/stripe-button";
 import CheckoutItem from "../../components/checkoutItem/checkoutItem";
 import "./checkoutPage.scss";
 
@@ -32,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">Total: {total}zł</div>
+    <div className="payments-alert">
+      <p>FOR USE PAYMENTS NEXT TEST CARD:</p>
+      <p>4242 4242 4242 4242 exp: 09/25 CVC:111 </p>
+    </div>
+
+    <TakeMoney price={total} />
   </div>
 );
 const mapStateToProps = createStructuredSelector({
